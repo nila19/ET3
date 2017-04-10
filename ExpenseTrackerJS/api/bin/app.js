16d82f7abac1ded995e150029132b80c6345583b
Bala Kuppusamy <bala.vigneswaran@gmail.com>
2017-04-10 18:46:56 -0500

    Moved connect logic to mongodb-config.

--- ExpenseTrackerJS/api/bin/app.js
+++ ExpenseTrackerJS/api/bin/app.js
@@ -9,46 +9,26 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // compresses the response..
 const compression = require('compression');
-
 // 'HTTP request' logging framework.
 const morgan = require('morgan');
 // blocking cross site attacks.
 const helmet = require('helmet');
-
 const express = require('express');
+
 const app = express();
 
-let billclosed = false;
-let okToLog = true;
 const httpSuccess = 400;  // less than 400 are success codes.
 
-const config = require('../config/config');
 const routes = require('../config/route-config');
 const mongo = require('../config/mongodb-config');
-const billcloser = require('../services/BillCloserService');
 
-const dbConnect = function () {
-  mongo.connect(app.locals.log, okToLog, function (err, db) {
-    // if connection error, print next connect msg.
-    okToLog = err ? true : false;
-    if(!err) {
-      app.locals.db = db;
-      // ensure billcloser runs only one time for every server start.
-      if(config.billcloser && !billclosed) {
-        billcloser.execute({db: app.locals.db, log: app.locals.log});
-        billclosed = true;
-      }
-    }
-  });
-  // keep trying every x seconds.
-  if(config.pulse.on) {
-    setTimeout(dbConnect, config.pulse.interval);
-  }
+const setCacheControl = function (res) {
+  res.setHeader('Cache-Control', 'public, max-age=31536000'); // 1 year
 };
 
 // store logger in app context for use from other components.
 app.locals.log = require('../utils/logger');
-dbConnect();
+mongo.connect(app);
 
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
@@ -64,7 +44,9 @@ app.use(morgan('dev', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../public'), {
+  setHeaders: setCacheControl
+}));
 app.use(favicon(path.join(__dirname, '../../public/images', 'favicon.ico')));
 
 // inject application routes...
--- ExpenseTrackerJS/api/config/mongodb-config.js
+++ ExpenseTrackerJS/api/config/mongodb-config.js
@@ -2,17 +2,40 @@
 
 const monk = require('monk');
 const config = require('./config');
+const billcloser = require('../services/BillCloserService');
+
+let okToLog = true;
+let billclosed = false;
+
+const ping = function (log, next) {
+  monk(config.dburl).then((db) => {
+    if(okToLog) {
+      log.info('Connected to :: ' + config.dburl);
+    }
+    next(null, db);
+  }).catch((err) => {
+    log.error(log.chalk.magenta(err));
+    next(err);
+  });
+};
+
+const connect = function (app) {
+  ping(app.locals.log, function (err, db) {
+    // if connection error, print next connect msg.
+    okToLog = Boolean(err);
+    app.locals.db = db;
+      // ensure billcloser runs only one time for every server start.
+    if(!err && config.billcloser && !billclosed) {
+      billcloser.execute({db: app.locals.db, log: app.locals.log});
+      billclosed = true;
+    }
+  });
+  // keep trying every x seconds.
+  if(config.pulse.on) {
+    setTimeout(connect, config.pulse.interval, app);
+  }
+};
 
 module.exports = {
-  connect: function (log, okToLog, next) {
-    monk(config.dburl).then((db) => {
-      if(okToLog) {
-        log.info('Connected to :: ' + config.dburl);
-      }
-      next(null, db);
-    }).catch((err) => {
-      log.error(log.chalk.magenta(err));
-      next(err);
-    });
-  }
+  connect: connect,
 };
