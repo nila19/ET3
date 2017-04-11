e42bc39125b715f0b5152971573a498dba754ded
Bala Kuppusamy <bala.vigneswaran@gmail.com>
2017-04-11 16:54:35 -0500

    Bug fix.

--- ExpenseTrackerJS/api/bin/socket-handler.js
+++ ExpenseTrackerJS/api/bin/socket-handler.js
@@ -1,4 +1,3 @@
-/* eslint no-console: "off" */
 'use strict';
 
 const PIPE = {
@@ -10,11 +9,9 @@ let app = null;
 
 const onConnect = function (ap) {
   app = ap;
-  app.locals.log.info('Socket connected...');
 };
 
 const publish = function (pipe, dt) {
-  console.log('Socket : ' + pipe + ' => ' + JSON.stringify(dt));
   app.locals.io.emit(pipe, {code: 0, data: dt});
 };
 
